bd84edcdc85e701d310cb24c541621c5905bfec7
Aaron Decker <decker49@gmail.com>
2020-04-22 07:38:05 -0500

    Create UI portion of 'create new user' screen.

--- src/cljs/video_note_taker/auth.cljs
+++ src/cljs/video_note_taker/auth.cljs
@@ -34,7 +34,9 @@
 (defn login [logged-in-atm]
   (let [user-atm (reagent/atom "alpha")
         pass-atm (reagent/atom "alpha")
-        login-failed-atm (reagent/atom false)]
+        pass-rpt-atm (reagent/atom "")
+        login-failed-atm (reagent/atom false)
+        creating-new-user? (reagent/atom false)]
     (fn []
       [:div {:class "flex flex-column items-center justify-center"}
        [:div {:class "f1 blue b ma3"} "Video Note Taker"]
@@ -45,21 +47,54 @@
         [:div {:class "flex items-center flex-wrap ma1"}
          [:div {:class ""} "Password"]
          [:input {:class "mh2" :type :text :value @pass-atm :on-change #(reset! pass-atm (-> % .-target .-value))}]]
-        [:div {:class "f2 br3 white bg-blue pa3 mv2 dim tc w5"
+        (when @creating-new-user?
+          [:div {:class "flex items-center flex-wrap ma1"}
+           [:div {:class ""} "Repeat password"]
+           [:input {:class "mh2" :type :text :value @pass-rpt-atm :on-change #(reset! pass-rpt-atm (-> % .-target .-value))}]])
+        (when (and @creating-new-user?
+;                   (not (empty? @pass-rpt-atm))
+                   (not (= @pass-atm @pass-rpt-atm)))
+          [:div {:class "f5 red"}
+           "Passwords do not match."])
+        (if @creating-new-user?
+          [:div {:class "f2 br3 white bg-blue pa3 mv2 dim tc w5"
                :on-click (fn []
                            (reset! login-failed-atm false)
-                           (go (let [resp (<! (http/post (db/resolve-endpoint "login")
-                                                         {:json-params {:user @user-atm
-                                                                        :pass @pass-atm}
-                                                          :with-credentials false} ;; no need to pass cookies while logging in -- the user shouldn't have our cookie at this point
-                                                         ))]
-                                 (if (:body resp)
-                                   (js/setTimeout #(swap! logged-in-atm inc) 200)
-                                   (reset! login-failed-atm true)))))}
-         "Login"]
+                           (if (= @pass-atm @pass-rpt-atm)
+                             (go (let [resp (<! (http/post (db/resolve-endpoint "create-user")
+                                                           {:json-params {:user @user-atm
+                                                                          :pass @pass-atm}
+                                                            :with-credentials false} ;; no need to pass cookies while logging in -- the user shouldn't have our cookie at this point
+                                                           ))]
+                                   (if (:body resp)
+                                     (js/setTimeout #(swap! logged-in-atm inc) 200)
+                                     (reset! login-failed-atm true))))
+                             (reset! login-failed-atm true)))}
+         "Create user"]
+          [:div {:class "f2 br3 white bg-blue pa3 mv2 dim tc w5"
+                 :on-click (fn []
+                             (reset! login-failed-atm false)
+                             (go (let [resp (<! (http/post (db/resolve-endpoint "login")
+                                                           {:json-params {:user @user-atm
+                                                                          :pass @pass-atm}
+                                                            :with-credentials false} ;; no need to pass cookies while creating a user -- the user shouldn't have our cookie at this point
+                                                           ))]
+                                   (if (:body resp)
+                                     (js/setTimeout #(swap! logged-in-atm inc) 200)
+                                     (reset! login-failed-atm true)))))}
+           "Login"])
+        (if @creating-new-user?
+          [:div {:class "f4 br3 ba b--black-10 pa3 mv4 dim tc w5"
+                 :on-click (fn [] (reset! creating-new-user? false))}
+           "... or use existing user"]
+          [:div {:class "f4 br3 ba b--black-10 pa3 mv4 dim tc w5"
+                 :on-click (fn [] (reset! creating-new-user? true))}
+           "... or create a new user"])
         (when @login-failed-atm
           [:div {:class "f3 br1 white bg-red b tc pa3 ma3"}
-           "Login failed :("])]])))
+           (if @creating-new-user?
+             "Couldn't create new user :("
+             "Login failed :(")])]])))
 
 (defn manage-identity [logged-in-atm]
   [:div
