28a7fa92a68c3d8bfd4cd57c236018248134e0ce
Aaron Decker <decker49@gmail.com>
2020-05-01 16:16:21 -0500

    Add 'type' attribute to video'.

--- src/cljs/video_note_taker/core.cljs
+++ src/cljs/video_note_taker/core.cljs
@@ -20,30 +20,31 @@
    [cljs.core.async.macros :refer [go go-loop]]))
 
 (defn video [video-ref-atm video-cursor video-options-cursor]
-  [:video {:id "main-video"
-           :class "mb3"
-           :controls true
-           :src  (db/resolve-endpoint (str "videos/" (:file-name @video-cursor)))
-           :width 620
-           :on-time-update (fn [e]
-                             (let [current-time   (.-currentTime (-> e .-target))
-                                   requested-time (:requested-time @video-options-cursor)]
-                               (println (:display-name @video-cursor)
-                                        " Time update event fired: current time"
-                                        current-time requested-time)
-                               (when requested-time
-                                 (if (< (Math/abs (- requested-time current-time)) 1)
-                                   (swap! video-options-cursor dissoc :requested-time)
-                                   (when-let [video @video-ref-atm]
-                                     (set! (.-currentTime video) requested-time))))))
-           :ref (fn [el]
-                  (when el
-                    (do
-                      (reset! video-ref-atm el)
-                      (when-let [requested-time (:requested-time @video-options-cursor)]
-                        (set! (.-currentTime el) requested-time)))))}
-   "Video not supported by your browser :("]
-  )
+  (let [file-ext (second (re-matches #".*\.(.*)" (:file-name @video-cursor)))]
+    [:video {:id "main-video"
+             :class "mb3"
+             :controls true
+             :src  (db/resolve-endpoint (str "videos/" (:file-name @video-cursor)))
+             :width 620
+             :type (str "video/" file-ext)
+             :on-time-update (fn [e]
+                               (let [current-time   (.-currentTime (-> e .-target))
+                                     requested-time (:requested-time @video-options-cursor)]
+                                 (println (:display-name @video-cursor)
+                                          " Time update event fired: current time"
+                                          current-time requested-time)
+                                 (when requested-time
+                                   (if (< (Math/abs (- requested-time current-time)) 1)
+                                     (swap! video-options-cursor dissoc :requested-time)
+                                     (when-let [video @video-ref-atm]
+                                       (set! (.-currentTime video) requested-time))))))
+             :ref (fn [el]
+                    (when el
+                      (do
+                        (reset! video-ref-atm el)
+                        (when-let [requested-time (:requested-time @video-options-cursor)]
+                          (set! (.-currentTime el) requested-time)))))}
+     "Video not supported by your browser :("]))
 
 (defn header [screen-cursor video-cursor]
   [:div {:class "flex items-center f4 justify-between w-100 bg-blue white mb1"}
