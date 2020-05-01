8bd401c231e06e4da2172ef3654d167a9a72abae
Aaron Decker <decker49@gmail.com>
2020-05-01 09:49:33 -0500

    Fix the hit target on the back button.

--- src/cljs/video_note_taker/core.cljs
+++ src/cljs/video_note_taker/core.cljs
@@ -50,8 +50,9 @@
    [:div {:class "flex items-center"}                             ; left side
     (if (= :video-selection (peek @screen-cursor))
       [:div {:class "b ma2"} "Video Note Taker"]
-      [:div {:class "bg-white br-100 pa1 ma1 dim"}
-       [svg/chevron-left {:on-click (fn [] (swap! screen-cursor pop))} "#357edd" "24px"]])
+      [:div {:class "bg-white br-100 pa1 ma1 dim"
+             :on-click (fn [] (swap! screen-cursor pop))}
+       [svg/chevron-left {} "#357edd" "24px"]])
     ;; (when (= :video (peek @screen-cursor))
     ;;   )
     (when (= :video (peek @screen-cursor))
