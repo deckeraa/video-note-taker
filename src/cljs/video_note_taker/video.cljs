86f170c280e71148294eabc8292935fda494e0cc
Aaron Decker <decker49@gmail.com>
2020-05-02 14:42:20 -0500

    Add test card for video

--- src/cljs/video_note_taker/video.cljs
+++ src/cljs/video_note_taker/video.cljs
@@ -1,31 +1,49 @@
 (ns video-note-taker.video
   (:require
    [reagent.core :as reagent]
-   [video-note-taker.db :as db]))
+   [video-note-taker.db :as db])
+  (:require-macros
+   [devcards.core :refer [defcard defcard-rg deftest]]
+   [cljs.core.async.macros :refer [go go-loop]]))
 
-(defn video [video-ref-atm video-cursor video-options-cursor]
-  (let [file-ext (second (re-matches #".*\.(.*)" (:file-name @video-cursor)))]
-    [:video {:id "main-video"
-             :class "mb3"
-             :controls true
-             :src  (db/resolve-endpoint (str "videos/" (:file-name @video-cursor)))
-             :width 620
-             :type (str "video/" file-ext)
-             :on-time-update (fn [e]
-                               (let [current-time   (.-currentTime (-> e .-target))
-                                     requested-time (:requested-time @video-options-cursor)]
-                                 (println (:display-name @video-cursor)
-                                          " Time update event fired: current time"
-                                          current-time requested-time)
-                                 (when requested-time
-                                   (if (< (Math/abs (- requested-time current-time)) 1)
-                                     (swap! video-options-cursor dissoc :requested-time)
-                                     (when-let [video @video-ref-atm]
-                                       (set! (.-currentTime video) requested-time))))))
-             :ref (fn [el]
-                    (when el
-                      (do
-                        (reset! video-ref-atm el)
-                        (when-let [requested-time (:requested-time @video-options-cursor)]
-                          (set! (.-currentTime el) requested-time)))))}
-     "Video not supported by your browser :("]))
+(defn video
+  ([video-ref-atm video-cursor video-options-cursor]
+   [video video-ref-atm video-cursor video-options-cursor {}])
+  ([video-ref-atm video-cursor video-options-cursor options]
+   (let [file-ext (second (re-matches #".*\.(.*)" (:file-name @video-cursor)))
+         src (if (:src-override options)
+               (db/resolve-endpoint (:src-override options))
+               (db/resolve-endpoint (str "videos/" (:file-name @video-cursor))))]
+     [:video {:id "main-video"
+              :class "mb3"
+              :controls true
+              :src  src
+              :width 620
+              :type (str "video/" file-ext)
+              :on-time-update (fn [e]
+                                (let [current-time   (.-currentTime (-> e .-target))
+                                      requested-time (:requested-time @video-options-cursor)]
+                                  (println (:display-name @video-cursor)
+                                           " Time update event fired: current time"
+                                           current-time requested-time)
+                                  (when requested-time
+                                    (if (< (Math/abs (- requested-time current-time)) 1)
+                                      (swap! video-options-cursor dissoc :requested-time)
+                                      (when-let [video @video-ref-atm]
+                                        (set! (.-currentTime video) requested-time))))))
+              :ref (fn [el]
+                     (when el
+                       (do
+                         (reset! video-ref-atm el)
+                         (when-let [requested-time (:requested-time @video-options-cursor)]
+                           (set! (.-currentTime el) requested-time)))))}
+      "Video not supported by your browser :("])))
+
+(defcard-rg video-card
+  (let [video-ref-atm (reagent/atom nil)
+        ; TODO check the test video into source
+        video-cursor  (reagent/atom {})
+        video-options-cursor (reagent/atom nil)
+        options {:src-override "A Tale of Two Kitties (1942).mp4"}]
+    [video video-ref-atm video-cursor video-options-cursor options]
+    ))
