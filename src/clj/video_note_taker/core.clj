c07d4157082f83d6c7da4acd7a809cdb7bf547fc
Aaron Decker <decker49@gmail.com>
2020-05-05 07:16:40 -0500

    Move auth functionality over into auth.clj.

--- src/clj/video_note_taker/core.clj
+++ src/clj/video_note_taker/core.clj
@@ -27,14 +27,14 @@
    [com.ashafa.clutch.http-client :refer [couchdb-request]]
    [clojure.data.json :as json]
    [clojure.java.shell :as shell :refer [sh]]
-   [clojure.walk :refer [keywordize-keys]]
    [clojure.java.io :as io]
    [clj-uuid :as uuid]
    [cljc.java-time.zoned-date-time :as zd]
    [cljc.java-time.duration :as dur]
    [clojure.data.csv :refer [read-csv write-csv]]
    [video-note-taker.search-shared :as search-shared :refer [construct-search-regex]]
-   [video-note-taker.upload-progress :as upload-progress])
+   [video-note-taker.upload-progress :as upload-progress]
+   [video-note-taker.auth :as auth :refer [wrap-cookie-auth]])
   (:gen-class))
 
 (def db
@@ -60,67 +60,6 @@
       (request/body-string)
       (json/read-str)
       (keywordize-keys)))
-
-(defn- remove-cookie-attrs-not-supported-by-ring
-  "CouchDB sends back cookie attributes like :version that ring can't handle. This removes those."
-  [cookies]
-  ;; CouchDB sends a cookies map that looks something like
-  ;; {AuthSession {:discard false, :expires #inst "2020-04-21T19:51:08.000-00:00", :path /, :secure false, :value YWxwaGE6NUU5RjQ5RkM6MXHV10hKUXVSuaY8GcMOZ2wFfeA, :version 0}}
-  (apply merge
-         (map (fn [[cookie-name v]]
-                (let [v (select-keys v [:value :domain :path :secure :http-only :max-age :same-site :expires])]
-                  (if (:expires v)
-                    {cookie-name (update v :expires #(.toString %))} ;; the :expires attr also needs changed frmo a java.util.Date to a string
-                    {cookie-name v})))
-              cookies)))
-
-(defn- set-cookies-flag [cookies flag flag-value]
-  (apply merge
-         (map (fn [[cookie-name v]]
-                {cookie-name (assoc v flag flag-value)})
-              cookies)))
-
-(deftest test-set-cookies-flag
-  (is (= (set-cookies-flag {"AuthSession"
-                                   {:value "YWxwaGE6NUVBQ0E1OUM6PNpD6s2El_yHqe2MNL-eOTGvkMQ"
-                                    :path "/"
-                                    :secure false
-                                    :expires "Fri May 01 18:41:32 CDT 2020"}}
-                                  :secure
-                                  true)
-         {"AuthSession"
-                                   {:value "YWxwaGE6NUVBQ0E1OUM6PNpD6s2El_yHqe2MNL-eOTGvkMQ"
-                                    :path "/"
-                                    :secure true
-                                    :expires "Fri May 01 18:41:32 CDT 2020"}}
-         )))
-
-(defn cookie-check
-  "Checks the cookies in a request against CouchDB. Returns [{:name :roles} new_cookie] if it's valid, false otherwise.
-  Note that
-    1) A new cookie being issued does not invalidate old cookies.
-    2) New cookies won't always be issued. It takes about a minute after getting a cookie before
-       CouchDB will give you a new cookie."
-  [cookie-value]
-  (let [
-        resp (http/get "http://localhost:5984/_session" {:as :json
-                                                         :headers {"Cookie" (str "AuthSession=" cookie-value)}
-                                                         :content-type :json
-                                                         })]
-    (if (nil? (get-in resp [:body :userCtx :name]))
-      false
-      [(get-in resp [:body :userCtx])
-       (remove-cookie-attrs-not-supported-by-ring (:cookies resp))])))
-
-(defn cookie-check-from-req [req]
-  (let [cookie-value (get-in req [:cookies "AuthSession" :value])]
-    (cookie-check cookie-value)))
-
-(defn cookie-check-handler [req]
-  (let [cookie-value (get-in req [:cookies "AuthSession" :value])]
-    (if-let [[userCtxt new-cookie] (cookie-check cookie-value)]
-      (assoc (json-response userCtxt) :cookies new-cookie)
-      (json-response false))))
 
 (defn put-doc-handler [req username]
   (let [doc (get-body req)]
@@ -250,14 +189,6 @@
 )
           (swap! failed-imports conj {:line line :reason "A note within one second of that timestamp already exists."}))))))
 
-(defn wrap-cookie-auth [handler]
-  (fn [req]
-    (let [cookie-check-val (cookie-check-from-req req)]
-      (if (not cookie-check-val)
-        (not-authorized-response)
-        (let [username (get-in cookie-check-val [0 :name])]
-          (handler req username))))))
-
 (defn download-starter-spreadsheet [req username]
   (println "download-starter-spreadsheet" username)
   (let [videos (couch/get-view db "videos" "by_user"
@@ -347,127 +278,6 @@
         ;; return the response
         (json-response {:success true})))
     ))
-
-(defn get-cookie-handler [req]
-  (try
-    (let [params (get-body req)
-          resp (http/post "http://localhost:5984/_session" {:as :json
-                                                            :content-type :json
-                                                            :form-params {:name (:user params)
-                                                                          :password (:pass params)}})]
-      (let [ring-resp
-            (assoc 
-             (json-response {:body (:body resp) :cookies (:cookies resp)})
-             :cookies (remove-cookie-attrs-not-supported-by-ring (:cookies resp)) ;; set the CouchDB cookie on the ring response
-             ;; :cookies {"secret" {:value "foobar", :secure true, :max-age 3600}}
-             )]
-                                        ;      (println ring-resp)
-        ring-resp))
-    (catch Exception e
-      (json-response {:result "login failed"}))))
-
-(defn login-handler [req]
-  (try
-    (let [params (get-body req)
-          resp (http/post "http://localhost:5984/_session" {:as :json
-                                                            :content-type :json
-                                                            :form-params {:name     (:user params)
-                                                                          :password (:pass params)}})]
-      (println "Login params: " params)
-      (println "Login resp: " resp)
-      (assoc 
-       (json-response true)
-       :cookies (as-> (:cookies resp) $
-                  (remove-cookie-attrs-not-supported-by-ring $)
-                  ;; (set-cookies-flag $ :secure true)
-                  ;; (set-cookies-flag $ :same-site :strict)
-                  (do (println $) $)
-                 )))
-    (catch Exception e
-      (json-response false))))
-
-(defn create-user-handler [req username]
-  (try
-    (let [params (get-body req)
-          name (:user params)]
-      (println "create-user-handler: " params)
-      (if (nil? (re-find #"^\w+$" name)) ; sanitize the name
-        (assoc (json-response :invalid-user-name) :status 400)
-        (let [resp (http/put
-                    (str "http://localhost:5984/_users/org.couchdb.user:" name)
-                    {:as :json
-                     :basic-auth [(:username db) (:password db)]
-                     :content-type :json
-                     :form-params {:name     name
-                                   :password (:pass params)
-                                   :roles []
-                                   :type :user}})]
-          (println "create-user resp: " resp)
-          (if (= 201 (:status resp))
-            (do
-              (let [login-resp (http/post "http://localhost:5984/_session" {:as :json
-                                                            :content-type :json
-                                                            :form-params {:name     (:user params)
-                                                                          :password (:pass params)}})]
-                (assoc 
-                 (json-response true)
-                 :cookies (remove-cookie-attrs-not-supported-by-ring (:cookies login-resp)) ;; set the CouchDB cookie on the ring response
-                 )))
-            (assoc (json-response false) :status 400) ;; don't want to leak any info useful to attackers, no keeping this very non-descript
-            ))))
-    (catch Exception e
-      (println "create-user-handler exception: " e)
-      (assoc (json-response false) :status 400))))
-
-(defn change-password-handler [req username]
-  (try
-    (let [params (get-body req)
-          cookie-value (get-in req [:cookies "AuthSession" :value])]
-      (let [old-user
-            (:body (http/get (str "http://localhost:5984/_users/org.couchdb.user:" username)
-                             {:as :json
-                              :headers {"Cookie" (str "AuthSession=" cookie-value)}}))
-            new-user (as-> old-user $
-                       (assoc $ :password (:pass params)))]
-        ;; change the password and then re-authenticate since the old cookie is no longer considered valid by CouchDB
-        (let [change-resp
-              (http/put (str "http://localhost:5984/_users/org.couchdb.user:" username)
-                        {:as :json
-                         :headers {"Cookie" (str "AuthSession=" cookie-value)}
-                         :content-type :json
-                         :form-params new-user})
-              new-login (http/post "http://localhost:5984/_session"
-                                   {:as :json
-                                    :content-type :json
-                                    :form-params {:name     username
-                                                  :password (:pass params)}})]
-          (assoc 
-           (json-response true)
-           :cookies (remove-cookie-attrs-not-supported-by-ring (:cookies new-login)) ;; set the CouchDB cookie on the ring response
-           ))
-        ))
-    (catch Exception e
-      (json-response false))))
-
-(defn logout-handler [req username]
-  (try
-    (let [resp (http/delete "http://localhost:5984/_session" {:as :json})]
-      (assoc 
-       (json-response {:logged-out true})
-       :cookies (remove-cookie-attrs-not-supported-by-ring (:cookies resp)) ;; set the CouchDB cookie on the ring response
-       )
-      )
-    (catch Exception e
-      (json-response {:logged-out false}))))
-
-(defn get-session-handler [req]
-  (let [cookie-value (get-in req [:cookies "AuthSession" :value])
-        resp (http/get "http://localhost:5984/_session" {:as :json
-                                                         :headers {"Cookie" (str "AuthSession=" cookie-value)}
-                                                         :content-type :json
-                                                         })
-        ]
-    (json-response {:body (:body resp)})))
 
 (defn search-text-handler [req username]
   (let [params (get-body req)
