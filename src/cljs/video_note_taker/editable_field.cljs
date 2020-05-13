71b08e60d0928c39aab8ae503ab6a5e9179109b1
Aaron Decker <decker49@gmail.com>
2020-05-13 08:25:06 -0500

    Make the editable-fields pencil only show up on hover.

--- src/cljs/video_note_taker/editable_field.cljs
+++ src/cljs/video_note_taker/editable_field.cljs
@@ -8,7 +8,8 @@
 (defn editable-field [initial-val save-fn]
   (let [editing? (reagent/atom false)
         restore-val-atm (reagent/atom initial-val)
-        val-atm (reagent/atom initial-val)]
+        val-atm (reagent/atom initial-val)
+        hover-atm (reagent/atom false)]
     (fn [initial-val save-fn]
       (if @editing?
         [:div {:class "flex items-center"}
@@ -33,14 +34,22 @@
                               (reset! val-atm @restore-val-atm)
                               (reset! editing? false))}
            "red" "24px"]]]
-        [:div {:class "flex items-center"}
-         [:p {:class ""} @val-atm]
-         [svg/pencil {:class "dim ma2"
-                      :on-click (fn [e]
-                                  (.stopPropagation e) ;; prevent this click from registing as a click on underlying elements.
-                                  (reset! editing? true)
-                                  (reset! restore-val-atm @val-atm))}
-          "gray" "18px"]]))))
+        [:div {:class "flex items-center"
+               :on-mouse-over (fn [e] (reset! hover-atm true))
+               :on-mouse-out  (fn [e] (reset! hover-atm false))}
+         [:p {:class ""}
+          @val-atm]
+         (if @hover-atm
+           [svg/pencil {:class "dim ma2"
+                        :on-click (fn [e]
+                                    (.stopPropagation e) ;; prevent this click from registing as a click on underlying elements.
+                                    (reset! editing? true)
+                                    (reset! restore-val-atm @val-atm))}
+            "gray" "18px"]
+           [:div {:class "ma2"
+                  :style {:width "18px"}}
+            ;; empty div to reserve space for the pencil effect on hover
+            ])]))))
 
 (defcard-rg editable-field-devcard
   [editable-field "Initial text for the field" (fn [] (println "save-fn!!"))])
