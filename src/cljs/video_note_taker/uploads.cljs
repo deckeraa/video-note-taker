d42d189788760b735b82d9048720f53dce455a30
Aaron Decker <decker49@gmail.com>
2020-05-18 08:07:15 -0500

    WIP: Create basic upload-display. Still needs hooked up into main app.

--- src/cljs/video_note_taker/uploads.cljs
+++ src/cljs/video_note_taker/uploads.cljs
@@ -6,7 +6,8 @@
    [cljs-http.client :as http]
    [cljs.core.async :refer [<! >! chan close! timeout put!] :as async]
    [video-note-taker.atoms :as atoms]
-   [video-note-taker.db :as db])
+   [video-note-taker.db :as db]
+   [video-note-taker.svg :as svg])
   (:require-macros
    [devcards.core :refer [defcard defcard-rg deftest]]
    [cljs.core.async.macros :refer [go go-loop]]))
@@ -68,18 +69,13 @@
          ;;                           repeat?)
          ;;                  (js/setTimeout )))
          ]
-     (println in-progress-uploads)
-     (println (map (fn [[k v]] [(:progress v)]) in-progress-uploads))
-     ;(println "keys: " (keys in-progress-uploads))
      (doall (map (fn [upload-id]
-                   (println "Sending message to endpoint: " upload-id)
                    (go (let [resp (<! (http/post
                                        (db/resolve-endpoint "get-upload-progress")
                                        {:query-params {:id upload-id}}))
                              updated-progress (:body resp)]
                          (if (= 200 (:status resp))
                            (do
-                             (println "Got updated progress: " updated-progress)
                              (swap! uploads-cursor assoc-in [upload-id :progress] updated-progress))                         
                            ("upload progress update failed: " resp)))))
                  (keys in-progress-uploads)))
@@ -87,7 +83,64 @@
   ([uploads-cursor]
    (upload-progress-updater uploads-cursor true)))
 
+;; Start the updater
 (defonce _upload-progress-updater (upload-progress-updater atoms/uploads-cursor))
+
+(defn- display-in-megabytes [bytes]
+  (if (>= bytes 1000000)
+    (str (Math/round (/ bytes 1000000)) " MB")
+    "<1 MB"))
+
+(deftest test-display-in-megabytes
+  (is (= (display-in-megabytes 50) "<1 MB"))
+  (is (= (display-in-megabytes 1000000) "1 MB"))
+  (is (= (display-in-megabytes 1500000) "2 MB")))
+
+(defn single-upload-card [upload-id {{:keys [bytes-read content-length]} :progress :as upload}]
+  ^{:key upload-id}
+  [:li {:class "ma3 flex flex-columns items-center"}
+   (if (and bytes-read content-length (= bytes-read content-length))
+     [svg/check {:class "ma2"} "green" "26px"]
+     [:div {:class "ma2" :style {:width "26px"}}] ;; TODO replace with loading spinner
+     )
+   [:div {:class "f3"}
+    (cond
+      ;; Case 1: Video has finished uploading
+      (and bytes-read content-length (= bytes-read content-length))
+      "Uploaded"
+      ;; Case 2: Video is uploading and we have progress information
+      (and bytes-read content-length)
+      (str "Uploading: "
+           (let [percent
+                 (Math/round (* (/ bytes-read content-length) 100))]
+             (if (number? percent) percent 0))
+           "%"
+           (str
+            " ("
+            (display-in-megabytes bytes-read)
+            " of "
+            (display-in-megabytes content-length)
+            ")"))
+      ;; Case 3: Video is uploading and we don't have progress information
+      :default
+      (str "Uploading..."))
+    [:div {:class "f4" :style {:overflow-wrap :anywhere}} (interpose ", " (:files upload))]]
+;   [:div {} (str (:progress upload))]
+   ])
+
+(defn upload-display [uploads-cursor]
+  (fn []
+    [:div {}
+     [:h1 "Uploads"]
+     [:ul {:class "list"}
+      (doall (map (fn [[k v]] (single-upload-card k v)) @uploads-cursor))]]))
+
+(defcard-rg test-upload-display
+  (let [uploads-cursor (reagent/atom {"dfh4" {:files ["foo.mp3" "bar.mp3" "a_really_really_long_file_name_123456789012334567890123456789012334567890123456789012334567890.mp4"] :progress {:bytes-read 100 :content-length 200}}
+                                        "823f" {:files ["qux.mp4"] :progress nil}
+                                        "4832" {:files ["baz.mp4"] :progress {:bytes-read 200 :content-length 200}}})]
+    (fn []
+      [upload-display uploads-cursor])))
 
 (defn- add-upload-to-cursor
   ([uploads-cursor file-input-ref-atom]
