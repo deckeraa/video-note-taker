161492b712067d9e96eeea0b794a12b9b60f5ff7
Aaron Decker <decker49@gmail.com>
2020-05-22 10:12:24 -0500

    Bump clj-http version to avoid the following error: WARNING: NoSuchMethodException was thrown when disabling normalizeUri. This indicates you are using an old version (< 4.5.8) of Apache http client. It is recommended to use http client version >= 4.5.9 to avoid the breaking change introduced in apache client 4.5.7 and the latency in exception handling. See https://github.com/aws/aws-sdk-java/issues/1919 for more information

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [ring-logger "1.0.1"]
                  [net.java.dev.jna/jna "5.5.0"]
                  [com.taoensso/timbre "4.11.0-alpha1"]
-                 [clj-http "3.9.1"]
+                 [clj-http "3.10.1"]
                  [cljs-http "0.1.46"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [danlentz/clj-uuid "0.1.9"]
