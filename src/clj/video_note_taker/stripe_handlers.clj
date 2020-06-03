d69d16bceb50596b4d2b76f644f9a0db6fe455e7
Aaron Decker <decker49@gmail.com>
2020-06-03 09:45:19 -0500

    Add ability to specify payment plan.

--- src/clj/video_note_taker/stripe_handlers.clj
+++ src/clj/video_note_taker/stripe_handlers.clj
@@ -2,16 +2,23 @@
   (:require
    [ring.util.json-response :refer [json-response]]
    [clj-stripe.common :as common]
-   [clj-stripe.checkouts :as checkouts]))
+   [clj-stripe.checkouts :as checkouts]
+   [video-note-taker.util :refer [get-body]]))
 
 (defn create-checkout-session-handler [req]
-  (let [secret-key (System/getenv "STRIPE_SECRET_KEY")]
+  (let [body (get-body req)
+        secret-key (System/getenv "STRIPE_SECRET_KEY")
+        plan (keyword (:plan body))]
     (println "Using STRIPE_SECRET_KEY: " secret-key)
+    (println "plan: " plan)
     (json-response 
      (common/with-token secret-key
        (common/execute
         (checkouts/create-checkout-session
-         [{:price-id "price_HOOdnszH3OSHgU" :quantity 1}]
+         (if (= :a plan)
+           [{:price-id "price_HOOd5cGclxAn2v"           :quantity 1}
+            {:price-id "price_1GpxiCBo2Vr1t1SegLl0iU1K" :quantity 1}]
+           [{:price-id "price_HOOdnszH3OSHgU"           :quantity 1}])
          "subscription"
          "http://localhost:3450/memories.html"
          "http://localhost:3450/?cancel=true"))))))
