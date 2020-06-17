78b0dff4575abe4caa8bf6ee6a5a217c3704eaa3
Aaron Decker <decker49@gmail.com>
2020-06-17 13:14:26 -0500

    Remove commented-out code.

--- src/clj/video_note_taker/groups.clj
+++ src/clj/video_note_taker/groups.clj
@@ -47,7 +47,7 @@
         affected-notes (get-notes (:_id video))]
     ;; TODO bulk update could fail on certain notes (for example, due to a document conflict),
     ;; so we'll need a search we can find periodically to validate the "index".
-    (warn "Updating affected-notes: " affected-notes video)
+    (info "Updating affected-notes via groups/denormalize-users-on-video: " affected-notes video)
     (db/bulk-update
      db nil
      (vec (map #(assoc % :users (vec all-users)) affected-notes))
@@ -70,36 +70,8 @@
                "execution_stats" true
                "limit" 125}
               affected-videos (:docs (db/run-mango-query videos-query (db/get-auth-cookie req)))
-              ;;indexed-videos (into {} (map (fn [doc] {(:_id doc) doc}) affected-videos))
-              ;;video-ids (mapv :_id affected-videos)
-              ;; notes-query
-              ;; {"selector"
-              ;;  {"$and" [{"type"
-              ;;            {"$eq" "note"}}
-              ;;           {"video"
-              ;;            {"$in" video-ids}}]}
-              ;;  "execution_stats" true
-              ;;  "limit" 1000}
-              ;; affected-notes (:docs (db/run-mango-query notes-query (db/get-auth-cookie req)))
               ]
-          (warn affected-videos)
-          ;;(warn "mapv ids: " video-ids)
-          ;;(warn "affected-notes: " affected-notes)
           (doall (map denormalize-users-on-video affected-videos))
-          ;; TODO re-denormalize :users attribute on affected notes here
-          
-            ;;               affected-notes (get-notes (:_id video) username roles (db/get-auth-cookie req))]
-            ;; ;; now update the denormalized user permissions stored on the notes
-          ;; (db/bulk-update
-          ;;  db nil
-          ;;  (vec (map (fn [note]
-          ;;              (assoc note :users (vec (clojure.set/union
-          ;;                                       (:users (get indexed-videos (:video note))) ;; existing users on the video
-          ;;                                       ;; the users from the group
-          ;;                                       ))))
-          ;;            affected-notes))
-          ;;  username roles (db/get-auth-cookie req))
-          
           (json-response updated-group))
         (util/not-authorized-response))
       (json-response (db/put-doc db access/put-hook-fn (merge req-group {:type "group" :created-by username :users []})
