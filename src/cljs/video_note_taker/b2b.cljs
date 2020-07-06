8578275eb7e94a1c9ca820d5676a1bc998302609
Aaron Decker <decker49@gmail.com>
2020-07-06 12:24:12 -0500

    Add in 'create customer' button.

--- src/cljs/video_note_taker/b2b.cljs
+++ src/cljs/video_note_taker/b2b.cljs
@@ -1,6 +1,28 @@
 (ns video-note-taker.b2b
-  (:require    [video-note-taker.atoms :as atoms]
-               [video-note-taker.svg :as svg]))
+  (:require
+   [reagent.core :as reagent]
+   [video-note-taker.atoms :as atoms]
+   [video-note-taker.svg :as svg]
+   [video-note-taker.auth :as auth]))
+
+(defn new-end-user-creation []
+  (let [username-atom (reagent/atom "")
+        validated-username-atom (reagent/atom "")]
+    (fn []
+      [:div
+       [:h2 {} "Create a new end-user"]
+       [auth/user-name-picker username-atom validated-username-atom]
+       [:button {:class (str "br3 white bn pa3 "
+                             (if (empty? @validated-username-atom)
+                               "bg-light-green"
+                               "bg-green dim"))
+                 :on-click (fn []
+                             (when (not (empty? @validated-username-atom))
+                               ;; user creation goes here
+                               ))}
+        "Create new end-user"]])))
 
 (defn business-view []
-  [:div "This is the business view."])
+  [:<>
+   [:div "This is the business view."]
+   [new-end-user-creation]])
