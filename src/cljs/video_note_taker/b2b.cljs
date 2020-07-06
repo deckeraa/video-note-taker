7bf3d2a4001cc4ae3c6167b95174af22264eacde
Aaron Decker <decker49@gmail.com>
2020-07-06 12:12:51 -0500

    Add b2b.cljs and show the business view instead of the videos view when a business user logs in.

---
+++ src/cljs/video_note_taker/b2b.cljs
@@ -0,0 +1,6 @@
+(ns video-note-taker.b2b
+  (:require    [video-note-taker.atoms :as atoms]
+               [video-note-taker.svg :as svg]))
+
+(defn business-view []
+  [:div "This is the business view."])
